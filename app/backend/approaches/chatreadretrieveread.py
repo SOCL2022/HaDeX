24e65216eb439281132f2c9375df66bff656f119
SOCL2022 <116817453+SOCL2022@users.noreply.github.com>
2020-05-30 12:12:33 +0000

    Updated po files

---
+++ app/backend/approaches/chatreadretrieveread.py
@@ -0,0 +1,53 @@
+
+import openai
+from azure.search.documents import SearchClient
+from azure.search.documents.models import QueryType
+from approaches.approach import Approach
+from text import nonewlines
+
+# Simple retrieve-then-read implementation, using the Cognitive Search and OpenAI APIs directly. It first retrieves
+# top documents from search, then constructs a prompt with them, and then uses OpenAI to generate an completion 
+# (answer) with that prompt.
+class ChatReadRetrieveReadApproach(Approach):
+    prompt_prefix = """<|im_start|>system
+Assistant helps the company employees with their healthcare plan questions, and questions about the employee handbook. Be brief in your answers.
+Answer ONLY with the facts listed in the list of sources below. If there isn't enough information below, say you don't know. Do not generate answers that don't use the sources below. If asking a clarifying question to the user would help, ask the question. 
+Each source has a name followed by colon and the actual information, always include the source name for each fact you use in the response. Use square brakets to reference the source, e.g. [info1.txt]. Don't combine sources, list each source separately, e.g. [info1.txt][info2.pdf].
+{follow_up_questions_prompt}
+{injected_prompt}
+Sources:
+{sources}
+<|im_end|>
+{chat_history}
+"""
+
+    follow_up_questions_prompt_content = """Generate three very brief follow-up questions that the user would likely ask next about their healthcare plan and employee handbook. 
+    Use double angle brackets to reference the questions, e.g. <<Are there exclusions for prescriptions?>>.
+    Try not to repeat questions that have already been asked.
+    Only generate questions and do not generate any text before or after the questions, such as 'Next Questions'"""
+
+    query_prompt_template = """Below is a history of the conversation so far, and a new question asked by the user that needs to be answered by searching in a knowledge base about employee healthcare plans and the employee handbook.
+    Generate a search query based on the conversation and the new question. 
+    Do not include cited source filenames and document names e.g info.txt or doc.pdf in the search query terms.
+    Do not include any text inside [] or <<>> in the search query terms.
+    If the question is not in English, translate the question to English before generating the search query.
+
+Chat History:
+{chat_history}
+
+Question:
+{question}
+
+Search query:
+"""
+
+    def __init__(self, search_client: SearchClient, chatgpt_deployment: str, gpt_deployment: str, sourcepage_field: str, content_field: str):
+        self.search_client = search_client
+        self.chatgpt_deployment = chatgpt_deployment
+        self.gpt_deployment = gpt_deployment
+        self.sourcepage_field = sourcepage_field
+        self.content_field = content_field
+
+    def run(self, history: list[dict], overrides: dict) -> any:
+        use_semantic_captions = True if overrides.get("semantic_captions") else False
+        top = overrides.get("top") or 3
