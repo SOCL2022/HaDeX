2550a084dc8f06d88eaa3b50287f42ed0003aff6
SOCL2022 <116817453+SOCL2022@users.noreply.github.com>
2020-06-02 12:55:09 +0000

    Updating SquashFS sort file

--- app/backend/approaches/readdecomposeask.py
+++ app/backend/approaches/readdecomposeask.py
@@ -1,4 +1,6 @@
 import openai
 from approaches.approach import Approach
 from azure.search.documents import SearchClient
-from azure.search.document
+from azure.search.documents.models import QueryType
+from langchain.llms.openai import AzureOpenAI
+from langchain.promp
