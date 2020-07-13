59ae6e4bb9e09b2e342312c2157e857ff65c9ed2
SOCL2022 <116817453+SOCL2022@users.noreply.github.com>
2020-07-13 19:12:52 +0000

    add first version

--- app/backend/approaches/readdecomposeask.py
+++ app/backend/approaches/readdecomposeask.py
@@ -70,4 +70,7 @@ class ReadDecomposeAsk(Approach):
         llm = AzureOpenAI(deployment_name=self.openai_deployment, temperature=overrides.get("temperature") or 0.3, openai_api_key=openai.api_key)
         tools = [
             Tool(name="Search", func=lambda q: self.search(q, overrides)),
-            Tool(n
+            Tool(name="Lookup", func=self.lookup)
+        ]
+
+        # Like results above, not great to keep this as a gl
