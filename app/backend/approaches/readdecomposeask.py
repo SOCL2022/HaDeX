535a1992dde09c3991dd00af72cd05ea258bb486
SOCL2022 <116817453+SOCL2022@users.noreply.github.com>
2020-07-30 16:05:36 +0000

    Whitespace changes only

--- app/backend/approaches/readdecomposeask.py
+++ app/backend/approaches/readdecomposeask.py
@@ -107,4 +107,5 @@ Thought 2: It does not mention the eastern sector. So I need to look up eastern
 sector.
 Action 2: Lookup[eastern sector]
 Observation 2: [info2.txt] (Result 1 / 1) The eastern sector extends into the High Plains and is called
-the Centr
+the Central Plains orogeny.
+Thought 3: The eastern sector of Colorado orogeny extends into the Hig
