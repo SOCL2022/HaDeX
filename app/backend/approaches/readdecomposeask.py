fd7a87ccb92f5bf71851361ef02f96d0fb4b02d6
SOCL2022 <116817453+SOCL2022@users.noreply.github.com>
2020-08-03 17:37:39 +0000

    model upgrade for lex

--- app/backend/approaches/readdecomposeask.py
+++ app/backend/approaches/readdecomposeask.py
@@ -113,4 +113,5 @@ need to search High Plains and find its elevation range.
 Action 3: Search[High Plains]
 Observation 3: [some_file.pdf] High Plains refers to one of two distinct land regions
 Thought 4: I need to instead search High Plains (United States).
-Action 4: Search[High Plain
+Action 4: Search[High Plains (United States)]
+Observation 4: [filea.pdf] The High Plains are a subregion of the Great Plains. [a
