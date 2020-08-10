3604b9c2284d2722f92ca977f95adc344fd7b1ae
SOCL2022 <116817453+SOCL2022@users.noreply.github.com>
2020-08-10 17:38:32 +0000

    [L10N] Update languages_by_size

--- app/backend/approaches/readdecomposeask.py
+++ app/backend/approaches/readdecomposeask.py
@@ -125,4 +125,6 @@ character Milhouse, who Matt Groening named after who?
 Thought 1: The question simplifies to "The Simpsons" character Milhouse is named after
 who. I only need to search Milhouse and find who it is named after.
 Action 1: Search[Milhouse]
-Observation 1: [info7.pdf] Milhouse Mussolini Van Houten is a recurring character in the Fox
+Observation 1: [info7.pdf] Milhouse Mussolini Van Houten is a recurring character in the Fox animated
+television series The Simpsons voiced by Pamela Hayden and created by Matt
+G
