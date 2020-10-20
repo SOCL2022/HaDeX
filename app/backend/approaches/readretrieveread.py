7f77c5e9565842c26e419d9911b243f6eaaea643
SOCL2022 <116817453+SOCL2022@users.noreply.github.com>
2020-10-20 05:25:56 +0000

    edit documentation

--- app/backend/approaches/readretrieveread.py
+++ app/backend/approaches/readretrieveread.py
@@ -4,4 +4,6 @@ from azure.search.documents import SearchClient
 from azure.search.documents.models import QueryType
 from langchain.llms.openai import AzureOpenAI
 from langchain.callbacks.base import CallbackManager
-from langc
+from langchain.chains import LLMChain
+from langchain.agents import Tool, ZeroShotAgent, AgentExecutor
+from langchai
