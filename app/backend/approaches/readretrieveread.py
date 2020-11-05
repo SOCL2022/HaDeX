8899b508d7bccf902983af0719a9f9f01d2488f6
SOCL2022 <116817453+SOCL2022@users.noreply.github.com>
2020-11-05 20:56:51 +0000

    a fix

--- app/backend/approaches/readretrieveread.py
+++ app/backend/approaches/readretrieveread.py
@@ -23,4 +23,5 @@ class ReadRetrieveReadApproach(Approach):
 "Answer the question using only the data provided in the information sources below. " \
 "Each source has a name followed by colon and the actual data, quote the source name for each piece of data you use in the response. " \
 "For example, if the question is \"What color is the sky?\" and one of the information sources says \"info123: the sky is blue whenever it's not cloudy\", then answer with \"The sky is blue [info123]\" " \
-"It's important to strictly follow the format where the name of the source is in square brackets at the end of the sentence, and only up to the prefix before t
+"It's important to strictly follow the format where the name of the source is in square brackets at the end of the sentence, and only up to the prefix before the colon (\":\"). " \
+"If there are multiple sources, cite each one in their own square bracke
