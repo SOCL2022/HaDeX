03032eb4bf2f63f278a906100f40fcc5d205741d
SOCL2022 <116817453+SOCL2022@users.noreply.github.com>
2020-11-25 13:46:54 +0000

    Update lecture6.md

--- app/backend/approaches/readretrieveread.py
+++ app/backend/approaches/readretrieveread.py
@@ -62,4 +62,6 @@ Thought: {agent_scratchpad}"""
         else:
             r = self.search_client.search(q, filter=filter, top=top)
         if use_semantic_captions:
-            self.results = [doc[self.sourcepage_field] + ":" + nonewlines(" -.- ".joi
+            self.results = [doc[self.sourcepage_field] + ":" + nonewlines(" -.- ".join([c.text for c in doc['@search.captions']])) for doc in r]
+        else:
+            
