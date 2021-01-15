54698743f7356b34bff1536f9753750ddb736b08
SOCL2022 <116817453+SOCL2022@users.noreply.github.com>
2021-01-15 14:30:38 +0000

    better formatting

--- app/backend/approaches/readretrieveread.py
+++ app/backend/approaches/readretrieveread.py
@@ -103,4 +103,8 @@ class EmployeeInfoTool(CsvLookupTool):
     employee_name: str = ""
 
     def __init__(self, employee_name: str):
-        super().__init__(filename = "data/employeeinfo.csv", key_field = "name", name = "Employee", description = "useful for answering questions about the employee, their benefits and other personal information
+        super().__init__(filename = "data/employeeinfo.csv", key_field = "name", name = "Employee", description = "useful for answering questions about the employee, their benefits and other personal information")
+        self.func = self.employee_info
+        self.employee_name = employee_name
+
+    def employe
