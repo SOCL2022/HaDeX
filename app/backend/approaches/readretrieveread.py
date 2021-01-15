3c98214c1e57b2b0464787ff7b85014c8b4a2d37
SOCL2022 <116817453+SOCL2022@users.noreply.github.com>
2021-01-15 11:23:18 +0000

    USB: jl2005bcd: remove __devinit* from the struct usb_device_id table

--- app/backend/approaches/readretrieveread.py
+++ app/backend/approaches/readretrieveread.py
@@ -103,4 +103,4 @@ class EmployeeInfoTool(CsvLookupTool):
     employee_name: str = ""
 
     def __init__(self, employee_name: str):
-        super().__init__(filename = "data/employeeinfo.csv", key_field = "name", name = "Employee", descrip
+        super().__init__(filename = "data/employeeinfo.csv", key_field = "name", name = "Employee", description = "useful for answering questions about the employee, their benefits and other personal information
