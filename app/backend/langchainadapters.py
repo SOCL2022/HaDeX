3eaefd156a962647a68a5b216d9e987039599de3
SOCL2022 <116817453+SOCL2022@users.noreply.github.com>
2021-01-22 15:04:42 +0000

    fix import bug

--- app/backend/langchainadapters.py
+++ app/backend/langchainadapters.py
@@ -4,4 +4,6 @@ from langchain.schema import AgentAction, AgentFinish, LLMResult
 
 def ch(text: str) -> str:
     s = text if isinstance(text, str) else str(text)
-    return s.replace("<", "&lt;").
+    return s.replace("<", "&lt;").replace(">", "&gt;").replace("\r", "").replace("\n", "<br>")
+
+class HtmlCallbackHandler (B
