bb8a0037583d10c1e32df22cbbecb7341312f559
SOCL2022 <116817453+SOCL2022@users.noreply.github.com>
2021-07-26 13:47:44 +0000

    Update: how-to-set-up-a-static-hugo-website-on-github-pages-in-2-minutes.md

--- app/frontend/src/api/api.ts
+++ app/frontend/src/api/api.ts
@@ -1,4 +1,7 @@
 import { AskRequest, AskResponse, ChatRequest } from "./models";
 
 export async function askApi(options: AskRequest): Promise<AskResponse> {
-    const response
+    const response = await fetch("/ask", {
+        method: "POST",
+        headers: {
+            "Content-Type": "
