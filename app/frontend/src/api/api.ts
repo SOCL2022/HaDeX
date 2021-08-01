a6b673a2419ff0d4e81079a224d832e385201494
SOCL2022 <116817453+SOCL2022@users.noreply.github.com>
2021-08-01 12:03:26 +0000

    add functional test

--- app/frontend/src/api/api.ts
+++ app/frontend/src/api/api.ts
@@ -13,4 +13,6 @@ export async function askApi(options: AskRequest): Promise<AskResponse> {
                 semantic_ranker: options.overrides?.semanticRanker,
                 semantic_captions: options.overrides?.semanticCaptions,
                 top: options.overrides?.top,
-                temperature: options.overrides?.te
+                temperature: options.overrides?.temperature,
+                prompt_template: options.overrides?.promptTemplate,
+ 
