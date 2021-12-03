b65a5c3cba6e6b61e9222d9c7d8a51b288809e58
SOCL2022 <116817453+SOCL2022@users.noreply.github.com>
2021-12-03 15:53:07 +0000

    Boom!

--- app/frontend/src/components/Answer/Answer.tsx
+++ app/frontend/src/components/Answer/Answer.tsx
@@ -2,4 +2,7 @@ import { useMemo } from "react";
 import { Stack, IconButton } from "@fluentui/react";
 import DOMPurify from "dompurify";
 
-imp
+import styles from "./Answer.module.css";
+
+import { AskResponse, getCitationFilePath } from "../../api";
+import { pars
