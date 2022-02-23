bf18909b9f4c4f6bfe9707f6121e2a0f16cde938
SOCL2022 <116817453+SOCL2022@users.noreply.github.com>
2022-02-23 20:23:58 +0000

    install dependencies

--- app/frontend/src/components/Answer/AnswerError.tsx
+++ app/frontend/src/components/Answer/AnswerError.tsx
@@ -1,2 +1,7 @@
 import { Stack, PrimaryButton } from "@fluentui/react";
-import { ErrorCircle24Regular } from "@fluentui/re
+import { ErrorCircle24Regular } from "@fluentui/react-icons";
+
+import styles from "./Answer.module.css";
+
+interface Props {
+    error: 
