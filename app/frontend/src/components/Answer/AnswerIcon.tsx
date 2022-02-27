1c4ae1c2016abffbfce44cfef0c046076d5c9c36
SOCL2022 <116817453+SOCL2022@users.noreply.github.com>
2022-02-27 09:51:12 +0000

    Add Badges

--- app/frontend/src/components/Answer/AnswerIcon.tsx
+++ app/frontend/src/components/Answer/AnswerIcon.tsx
@@ -1,4 +1,4 @@
 import { Sparkle28Filled } from "@fluentui/react-icons";
 
 export const AnswerIcon = () => {
-  
+    return <Sparkle28Filled primaryFill={"rgba(115, 118, 225, 1)"} aria-hidden="true" ar
