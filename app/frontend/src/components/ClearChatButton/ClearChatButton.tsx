5eb75f422ab0637ec8b18ce1003771a2d4711c0c
SOCL2022 <116817453+SOCL2022@users.noreply.github.com>
2022-03-24 10:29:26 +0000

    Update html/settings/_settings.scss

--- app/frontend/src/components/ClearChatButton/ClearChatButton.tsx
+++ app/frontend/src/components/ClearChatButton/ClearChatButton.tsx
@@ -1,4 +1,9 @@
 import { Text } from "@fluentui/react";
 import { Delete24Regular } from "@fluentui/react-icons";
 
-import styles 
+import styles from "./ClearChatButton.module.css";
+
+interface Props {
+    className?: string;
+    onClick: () => void;
+    disab
