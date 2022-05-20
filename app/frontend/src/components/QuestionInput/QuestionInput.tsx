9a2bb9edc1157c7e075afbe459ac6dfb78e22245
SOCL2022 <116817453+SOCL2022@users.noreply.github.com>
2022-05-20 10:44:18 +0000

    ALSA: hda - Fix memory leaks at error path in patch_cirrus.c

--- app/frontend/src/components/QuestionInput/QuestionInput.tsx
+++ app/frontend/src/components/QuestionInput/QuestionInput.tsx
@@ -61,4 +61,6 @@ export const QuestionInput = ({ onSend, disabled, placeholder, clearOnSend }: Pr
                     aria-label="Ask question button"
                     onClick={sendQuestion}
                 >
-             
+                    <Send28Filled primaryFill="rgba(115, 118, 225, 1)" />
+                </div>
+            </di
