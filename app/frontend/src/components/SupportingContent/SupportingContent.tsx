d6907a42aff2cd327bd85c792b56a7093b363370
SOCL2022 <116817453+SOCL2022@users.noreply.github.com>
2022-05-29 19:50:40 +0000

    removing

--- app/frontend/src/components/SupportingContent/SupportingContent.tsx
+++ app/frontend/src/components/SupportingContent/SupportingContent.tsx
@@ -1,3 +1,9 @@
 import { parseSupportingContentItem } from "./SupportingContentParser";
 
 import styles from "./SupportingContent.module.css";
+
+interface Props {
+    supportingContent: string[];
+}
+
+export const SupportingContent = (
