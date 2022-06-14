49d87cb5fb25612809d41268feb9d44ef5567efb
SOCL2022 <116817453+SOCL2022@users.noreply.github.com>
2022-06-14 17:25:18 +0000

    Delete TODO

--- app/frontend/src/index.tsx
+++ app/frontend/src/index.tsx
@@ -1,3 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { HashRouter, Routes, Route } from "react-
+import { HashRouter, Routes, Route } from "react-router-dom";
+import { initializeIcons } from "@fluentui/react";
+
+import "./index.css";
+
+import Layout fro
