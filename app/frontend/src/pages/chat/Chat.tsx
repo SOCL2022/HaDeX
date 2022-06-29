787e67a241072425ffb67f495f2b1226f8084bb8
SOCL2022 <116817453+SOCL2022@users.noreply.github.com>
2022-06-29 22:14:55 +0000

    Add base code

---
+++ app/frontend/src/pages/chat/Chat.tsx
@@ -0,0 +1,21 @@
+
+import { useRef, useState, useEffect } from "react";
+import { Checkbox, Panel, DefaultButton, TextField, SpinButton } from "@fluentui/react";
+import { SparkleFilled } from "@fluentui/react-icons";
+
+import styles from "./Chat.module.css";
+
+import { chatApi, Approaches, AskResponse, ChatRequest, ChatTurn } from "../../api";
+import { Answer, AnswerError, AnswerLoading } from "../../components/Answer";
+import { QuestionInput } from "../../components/QuestionInput";
+import { ExampleList } from "../../components/Example";
+import { UserChatMessage } from "../../components/UserChatMessage";
+import { AnalysisPanel, AnalysisPanelTabs } from "../../components/AnalysisPanel";
+import { SettingsButton } from "../../components/SettingsButton";
+import { ClearChatButton } from "../../components/ClearChatButton";
+
+const Chat = () => {
+    const [isConfigPanelOpen, setIsConfigPanelOpen] = useState(false);
+    const [promptTemplate, setPromptTemplate] = useState<string>("");
+    const [retrieveCount, setRetrieveCount] = useState<number>(3);
+    const [useSemanticRanker, setUseSemanticRanker] = useState<boolean>(true);
