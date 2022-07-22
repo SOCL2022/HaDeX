ddbfec3a08b00b81394f7b387fe1b2b119819565
SOCL2022 <116817453+SOCL2022@users.noreply.github.com>
2022-07-22 08:01:31 +0000

    finalized

--- app/frontend/src/pages/layout/Layout.tsx
+++ app/frontend/src/pages/layout/Layout.tsx
@@ -1,3 +1,8 @@
 import { Outlet, NavLink, Link } from "react-router-dom";
 
-import github from "../.
+import github from "../../assets/github.svg";
+
+import styles from "./Layout.module.css";
+
+const Layout = () => {
+    return 
