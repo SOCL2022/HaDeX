4de815a50f140ccf86a8a444a24302caab7b6b9e
SOCL2022 <116817453+SOCL2022@users.noreply.github.com>
2022-08-14 11:01:17 +0000

    ce

---
+++ app/frontend/src/pages/oneshot/OneShot.tsx
@@ -0,0 +1,50 @@
+
+import { useRef, useState } from "react";
+import { Checkbox, ChoiceGroup, IChoiceGroupOption, Panel, DefaultButton, Spinner, TextField, SpinButton } from "@fluentui/react";
+
+import styles from "./OneShot.module.css";
+
+import { askApi, Approaches, AskResponse, AskRequest } from "../../api";
+import { Answer, AnswerError } from "../../components/Answer";
+import { QuestionInput } from "../../components/QuestionInput";
+import { ExampleList } from "../../components/Example";
+import { AnalysisPanel, AnalysisPanelTabs } from "../../components/AnalysisPanel";
+import { SettingsButton } from "../../components/SettingsButton/SettingsButton";
+
+const OneShot = () => {
+    const [isConfigPanelOpen, setIsConfigPanelOpen] = useState(false);
+    const [approach, setApproach] = useState<Approaches>(Approaches.RetrieveThenRead);
+    const [promptTemplate, setPromptTemplate] = useState<string>("");
+    const [promptTemplatePrefix, setPromptTemplatePrefix] = useState<string>("");
+    const [promptTemplateSuffix, setPromptTemplateSuffix] = useState<string>("");
+    const [retrieveCount, setRetrieveCount] = useState<number>(3);
+    const [useSemanticRanker, setUseSemanticRanker] = useState<boolean>(true);
+    const [useSemanticCaptions, setUseSemanticCaptions] = useState<boolean>(false);
+    const [excludeCategory, setExcludeCategory] = useState<string>("");
+
+    const lastQuestionRef = useRef<string>("");
+
+    const [isLoading, setIsLoading] = useState<boolean>(false);
+    const [error, setError] = useState<unknown>();
+    const [answer, setAnswer] = useState<AskResponse>();
+
+    const [activeCitation, setActiveCitation] = useState<string>();
+    const [activeAnalysisPanelTab, setActiveAnalysisPanelTab] = useState<AnalysisPanelTabs | undefined>(undefined);
+
+    const makeApiRequest = async (question: string) => {
+        lastQuestionRef.current = question;
+
+        error && setError(undefined);
+        setIsLoading(true);
+        setActiveCitation(undefined);
+        setActiveAnalysisPanelTab(undefined);
+
+        try {
+            const request: AskRequest = {
+                question,
+                approach,
+                overrides: {
+                    promptTemplate: promptTemplate.length === 0 ? undefined : promptTemplate,
+                    promptTemplatePrefix: promptTemplatePrefix.length === 0 ? undefined : promptTemplatePrefix,
+                    promptTemplateSuffix: promptTemplateSuffix.length === 0 ? undefined : promptTemplateSuffix,
+                    excludeCategory: excludeCategory.length === 0 ? undefined : excludeCategory,
