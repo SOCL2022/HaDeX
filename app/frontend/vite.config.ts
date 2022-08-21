214a7cb3b5b8a4d22a4d50a815c899ce238dceec
SOCL2022 <116817453+SOCL2022@users.noreply.github.com>
2022-08-21 15:57:38 +0000

    Update the release notes

--- app/frontend/vite.config.ts
+++ app/frontend/vite.config.ts
@@ -3,4 +3,8 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react
+    plugins: [react()],
+    build: {
+        outDir: "../backend/static",
+        emptyOutDir: true,
+        s
