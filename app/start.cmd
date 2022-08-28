29e44d9ef8981934284bf7fdbfc017c5ff52e17f
SOCL2022 <116817453+SOCL2022@users.noreply.github.com>
2022-08-28 19:03:30 +0000

    Automatic patch number increase [ci skip]

--- app/start.cmd
+++ app/start.cmd
@@ -6,4 +6,9 @@ echo Loading azd .env file from current environment
 echo.
 
 @echo off
-for /f "tokens=1* delims==" %%a in ('azd env
+for /f "tokens=1* delims==" %%a in ('azd env get-values') do (
+  set "%%a=%%~b"
+)
+
+if "%errorlevel%" neq "0" (
+    echo Failed to load environm
