48b39a1f0691a45aae382e744880350461bfd63b
SOCL2022 <116817453+SOCL2022@users.noreply.github.com>
2023-03-04 12:49:05 +0000

    more plots

--- scripts/prepdocs.py
+++ scripts/prepdocs.py
@@ -14,3 +14,98 @@ from azure.search.documents.indexes.models import *
 from azure.search.documents import SearchClient
 
 MAX_SECTION_LENGTH = 1000
+SENTENCE_SEARCH_LIMIT = 100
+SECTION_OVERLAP = 100
+
+parser = argparse.ArgumentParser(
+    description="Prepare documents by extracting content from PDFs, splitting content into sections, uploading to blob storage, and indexing in a search index.",
+    epilog="Example: prepdocs.py '..\data\*' --storageaccount myaccount --container mycontainer --searchservice mysearch --index myindex -v"
+    )
+parser.add_argument("files", help="Files to be processed")
+parser.add_argument("--category", help="Value for the category field in the search index for all sections indexed in this run")
+parser.add_argument("--skipblobs", action="store_true", help="Skip uploading individual pages to Azure Blob Storage")
+parser.add_argument("--storageaccount", help="Azure Blob Storage account name")
+parser.add_argument("--container", help="Azure Blob Storage container name")
+parser.add_argument("--storagekey", required=False, help="Optional. Use this Azure Blob Storage account key instead of the current user identity to login (use az login to set current user for Azure)")
+parser.add_argument("--searchservice", help="Name of the Azure Cognitive Search service where content should be indexed (must exist already)")
+parser.add_argument("--index", help="Name of the Azure Cognitive Search index where content should be indexed (will be created if it doesn't exist)")
+parser.add_argument("--searchkey", required=False, help="Optional. Use this Azure Cognitive Search account key instead of the current user identity to login (use az login to set current user for Azure)")
+parser.add_argument("--remove", action="store_true", help="Remove references to this document from blob storage and the search index")
+parser.add_argument("--removeall", action="store_true", help="Remove all blobs from blob storage and documents from the search index")
+parser.add_argument("--verbose", "-v", action="store_true", help="Verbose output")
+args = parser.parse_args()
+
+# Use the current user identity to connect to Azure services unless a key is explicitly set for any of them
+default_creds = DefaultAzureCredential() if args.searchkey == None or args.storagekey == None else None
+search_creds = default_creds if args.searchkey == None else AzureKeyCredential(args.searchkey)
+if not args.skipblobs:
+    storage_creds = default_creds if args.storagekey == None else args.storagekey
+
+def blob_name_from_file_page(filename, page):
+    return os.path.splitext(os.path.basename(filename))[0] + f"-{page}" + ".pdf"
+
+def upload_blobs(pages):
+    blob_service = BlobServiceClient(account_url=f"https://{args.storageaccount}.blob.core.windows.net", credential=storage_creds)
+    blob_container = blob_service.get_container_client(args.container)
+    if not blob_container.exists():
+        blob_container.create_container()
+    for i in range(len(pages)):
+        blob_name = blob_name_from_file_page(filename, i)
+        if args.verbose: print(f"\tUploading blob for page {i} -> {blob_name}")
+        f = io.BytesIO()
+        writer = PdfWriter()
+        writer.add_page(pages[i])
+        writer.write(f)
+        f.seek(0)
+        blob_container.upload_blob(blob_name, f, overwrite=True)
+
+def remove_blobs(filename):
+    if args.verbose: print(f"Removing blobs for '{filename or '<all>'}'")
+    blob_service = BlobServiceClient(account_url=f"https://{args.storageaccount}.blob.core.windows.net", credential=storage_creds)
+    blob_container = blob_service.get_container_client(args.container)
+    if blob_container.exists():
+        if filename == None:
+            blobs = blob_container.list_blob_names()
+        else:
+            prefix = os.path.splitext(os.path.basename(filename))[0]
+            blobs = filter(lambda b: re.match(f"{prefix}-\d+\.pdf", b), blob_container.list_blob_names(name_starts_with=os.path.splitext(os.path.basename(prefix))[0]))
+        for b in blobs:
+            if args.verbose: print(f"\tRemoving blob {b}")
+            blob_container.delete_blob(b)
+
+def split_text(pages):
+    SENTENCE_ENDINGS = [".", "!", "?"]
+    WORDS_BREAKS = [",", ";", ":", " ", "(", ")", "[", "]", "{", "}", "\t", "\n"]
+    if args.verbose: print(f"Splitting '{filename}' into sections")
+
+    page_map = []
+    offset = 0
+    for i, p in enumerate(pages):
+        text = p.extract_text()
+        page_map.append((i, offset, text))
+        offset += len(text)
+
+    def find_page(offset):
+        l = len(page_map)
+        for i in range(l - 1):
+            if offset >= page_map[i][1] and offset < page_map[i + 1][1]:
+                return i
+        return l - 1
+
+    all_text = "".join(p[2] for p in page_map)
+    length = len(all_text)
+    start = 0
+    end = length
+    while start + SECTION_OVERLAP < length:
+        last_word = -1
+        end = start + MAX_SECTION_LENGTH
+
+        if end > length:
+            end = length
+        else:
+            # Try to find the end of the sentence
+            while end < length and (end - start - MAX_SECTION_LENGTH) < SENTENCE_SEARCH_LIMIT and all_text[end] not in SENTENCE_ENDINGS:
+                if all_text[end] in WORDS_BREAKS:
+                    last_word = end
+                end += 1
+            if end < length and all_text[end] not in SENTENCE_ENDINGS and last_word > 0:
